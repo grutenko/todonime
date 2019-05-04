4fac0474a7db04feba56afac3acb96dd09e32ecf
Alexey Grigorev <alexfedor932@gmail.com>
2019-05-04 19:39:52 +0300

    Setting prod/dev environment

--- webpack.config.js
+++ webpack.config.js
@@ -3,38 +3,40 @@ const webpack = require('webpack');
 const CompressionPlugin = require("compression-webpack-plugin");
 const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-	mode: "development",
-	entry: {
-		content: "./src/index.js",
-		background: "./src/entries/background.js",
-		settings: "./src/entries/settings.js"
-	},
-	output: {
-		path: path.resolve(__dirname, "public/js"),
-		filename: "[name].js",
-		publicPath: "/public/assets"
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader"
+module.exports = (env, argv) => {
+	return {
+		mode: process.env.NODE_ENV || "development",
+		entry: {
+			content: "./src/index.js",
+			background: "./src/entries/background.js",
+			settings: "./src/entries/settings.js"
+		},
+		output: {
+			path: path.resolve(__dirname, "public/js"),
+			filename: "[name].js",
+			publicPath: "/public/assets"
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "babel-loader"
+					}
 				}
-			}
+			]
+		},
+		plugins: [
+			new webpack.ProvidePlugin({
+				$: 'jquery',
+				jQuery: 'jquery'
+			}),
+			new Dotenv({
+				path: argv.mode == "production"
+					? "./.env.production"
+					: "./.env.development"
+			})
 		]
-	},
-	plugins: [
-		new webpack.ProvidePlugin({
-			$: 'jquery',
-			jQuery: 'jquery'
-		}),
-		new Dotenv({
-			path: process.env.mode == "production"
-				? "./.env.production"
-				: "./.env.development"
-		})
-	],
+	}
 };
