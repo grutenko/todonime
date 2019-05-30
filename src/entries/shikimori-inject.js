9970c9279d09fdd3e5dfb4124d3d5e18fdb0ab2c
Alexey Grigorev <alexfedor932@gmail.com>
2019-05-30 09:36:23 +0300

    Make watching button in shikimori.one

---
+++ src/entries/shikimori-inject.js
@@ -0,0 +1,41 @@
+var store = {};
+
+function send(message, callback) {
+    const storeID = Math.floor(Math.random()*1000);
+    chrome.runtime.sendMessage(Object.assign(message, {storeID}));
+    
+    store[storeID] = callback;
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    store[request.storeID](request);
+});
+
+function createButton(url) {
+	var button = $('<section></section>')
+		.addClass('block')
+		.addClass('watch-online-block');
+
+	button.html('<a class="play-button" href="'+url+'">'
+		+'<img src="https://todonime.space/images/logotype-48x48.png" style="width:24px;height: 24px">'
+		+'<span class="play-button-text">Смотреть онлайн</span></a>');
+	$('.watch-online-placeholer').html(button);
+}
+
+const url = location.href.match(/\/+animes\/+[a-z]?(\d+)-([a-z0-9\-]+)/);
+if(url[1]) {
+	send({
+			c: 'get-anime',
+			data: {anime_id: url[1]}
+		},
+		({anime}) => {
+			const rate = anime.user_rate;
+			const episode = !rate
+				? 1
+				: ((anime.status == 'ongoing' && rate.episodes < anime.episodes_aired) || (anime.status == 'released' && rate.episodes < anime.episodes)
+						? rate.episodes + 1
+						: rate.episodes);
+
+			createButton('https://todonime.space/video/'+url[1]+'/' + episode + '?back='+location.href)
+		});
+}
