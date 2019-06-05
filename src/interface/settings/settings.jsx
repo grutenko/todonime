e8d8f470b6d9449b4f02d40f166d6937f7ab5442
Alexey Grigorev <alexfedor932@gmail.com>
2019-06-05 23:02:23 +0300

    - Add "in development" message

--- src/interface/settings/settings.jsx
+++ src/interface/settings/settings.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 export default class Settings extends Component {
 	render() {
-		return (<div></div>);
+		return (<div><h2>Находится в разработке.</h2></div>);
 	}
 }
