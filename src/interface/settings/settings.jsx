66a3fbe136c43f29e70d64974bc74d9c1e452c0e
Alexey Grigorev <alexfedor932@gmail.com>
2019-07-23 10:11:41 +0300

    * Create settings page

--- src/interface/settings/settings.jsx
+++ src/interface/settings/settings.jsx
@@ -1,8 +1,20 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
+import Tabs from '../tabs/tabs';
+import GeneralSettings from '../tabs/general-settings';
+import AccountSettings from '../tabs/account-settings';
+import DesignSettings from '../tabs/design-settings';
+
 export default class Settings extends Component {
 	render() {
-		return (<div><h2>Находится в разработке.</h2></div>);
+		return (
+			<div className="app__container">
+				<Tabs activeTab={0} showSettingsButton={false} showTitle={false}>
+					<GeneralSettings iconName="Основные" name="Основные"/>
+					<DesignSettings iconName="Дизайн" name="Дизайн"/>
+					<AccountSettings iconName="Аккаунт" name="Аккаунт"/>
+				</Tabs>
+			</div>);
 	}
 }
