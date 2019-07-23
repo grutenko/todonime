77a6ff0bdd3fc4bf2d708cde72c292a20510a891
Alexey Grigorev <alexfedor932@gmail.com>
2019-07-23 10:13:18 +0300

    - Used tabs supporting

--- src/app.js
+++ src/app.js
@@ -11,18 +11,42 @@ import OnHoldTab from './interface/tabs/on-hold';
 
 import {get} from './lib/settings';
 
+const TABS_KEYS = [
+	'planned',
+	'watching',
+	'rewatching',
+	'on_hold',
+	'completed',
+	'dropped',
+];
+
+const ALL_TABS = {
+	'planned': <PlannedTab key={1} iconName="add" name="Запланировано" auth="true"/>,
+	'watching': <WatchTab key={2} iconName="play_arrow" name="Смотрю" auth="true"/>,
+	'rewatching': <RewatchTab key={3} iconName="replay" name="Пересматриваю" auth="true"/>,
+	'on_hold': <OnHoldTab key={4} iconName="pause" name="Отложено" auth="true"/>,
+	'completed': <CompleteTab key={5} iconName="check" name="Просмотрено" auth="true"/>,
+	'dropped': <DroppedTab key={6} iconName="delete" name="Брошено" auth="true"/>,
+};
+
 
 export default class App extends Component {
 	render() {
+		const tabKeys = get('usedTabs') || TABS_KEYS;
+
 		return (
 			<div className="app__container">
-				<Tabs activeTab={get('activeTab') != null ? get('activeTab') : 1} showSettingsButton={true}>
-					<PlannedTab iconName="add" name="Запланировано" auth="true"/>
-					<WatchTab iconName="play_arrow" name="Смотрю" auth="true"/>
-					<RewatchTab iconName="replay" name="Пересматриваю" auth="true"/>
-					<OnHoldTab iconName="pause" name="Отложено" auth="true"/>
-					<CompleteTab iconName="check" name="Просмотрено" auth="true"/>
-					<DroppedTab iconName="delete" name="Брошено" auth="true"/>
+				<Tabs
+					activeTab={get('activeTab') || get('defaultActiveTab') || 1}
+					showSettingsButton={get('showSettingsButton') || false}
+					showTitle={true}
+				>
+					{
+						Object.entries(ALL_TABS).filter(([key, value]) => {
+							return tabKeys.indexOf(key) != -1;
+						})
+						.map(([key, value]) => value)
+					}
 				</Tabs>
 			</div>
 		)
